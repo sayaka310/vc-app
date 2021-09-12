0e23dab69cb3a8b7374d4a83bd1357f1b95c1378
sayaka310 <syk3887@yahoo.co.jp>
2021-09-12 14:20:02 +0900

    2nd commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::Httpメゾッド(URI,ACTION);
+Route::get('/route/hello', function () {
+    return '<h1>Hell from Route!';
+});
+
+Route::get('view/hello', function () {
+    return view('message.hello');
+
+});
+
+Route::get('/view/ver', function () {
+    return view('message.var',['variable' 
+=> '$variable']);
+});
+
+Route::get('view/word/{msg}', function ($msg) {
+    return view('massage.word', ['msg' => $msg]);
+});
+
+Route::get('view/word/{name}/{msg}', function($name, $msg) {
+    return view('message.word', [
+        'name' => $name,
+        'msg' => $msg
+    ]);
+});
+
+Route::get('controller/hello', [App\Http\Controllers\MessageContoroller::class, 'hell']);
+Route::get('/controller/var', [App\Http\Controllers\MessageController::class, 'var']);
+Route::get('/controller/word/{msg}', [App\Http\Controllers\MessageController::class, 'word']);
+Route::get('/controller/word/{name}/{msg}', [App\Http\Controllers\MessageController::class, 'word2']);
+
+Route::get('/language', [App\Http\Controllers\LanguageController::class,'index' ]);
